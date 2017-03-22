e84529c0aca896f57c1df485e0038eb51bd8a43e
Axelle Renie <axelle.renie@gmail.com>
2017-03-22 13:09:40 +0100

    index+migration2

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,12 @@
 class EventsController < ApplicationController
   def index
+    @selected_categories = params[:categories].presence || Category.pluck(:name)
     @categories = Category.all
-    @events = policy_scope(Event).order(created_at: :desc)
+
+    events = policy_scope(Event).
+      joins(:category).
+      where(categories: { name: @selected_categories }).
+      order(created_at: :desc)
 
     @hash = {}
     date = Time.now.to_date - 1.days
@@ -9,8 +14,8 @@ class EventsController < ApplicationController
       @hash[date += 1.day] = []
     end
 
-    @hash.each do |date, events|
-      @hash[date] << Event.where(occurs_at: date)
+    @hash.each do |date, _date_events|
+      @hash[date] << events.where(occurs_at: date)
       @hash[date].flatten!
     end
 
