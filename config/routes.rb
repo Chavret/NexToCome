59a380f644dd31aed5f6afe8e4c802020cb52f45
Axelle Renie <axelle.renie@gmail.com>
2017-03-23 21:38:10 +0100

    style

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-
-
   get 'admin', to: 'events#admin'
   root to: 'events#index'
 
