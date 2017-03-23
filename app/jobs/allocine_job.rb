769e8fb7bad94d098949eee29c75956056b8847c
Leo Dubert <leodubert@gmail.com>
2017-03-23 23:39:22 +0100

    managed to automoise scraper

--- app/jobs/allocine_job.rb
+++ app/jobs/allocine_job.rb
@@ -6,8 +6,9 @@ require 'nokogiri'
 class AllocineJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
+  def perform
     # Do something later
+
     t = Time.now
     4.times do
     t += 7*24*60*60
@@ -20,24 +21,31 @@ class AllocineJob < ApplicationJob
 
 
   def allocine_week_scraper(html_file)
-
+    categorie = Category.find_by_name('Economie')
     html_doc = Nokogiri::HTML(html_file)
-
-    p html_doc.search('.card-entity-list')
 
     html_doc.search('.card-entity-list').each do |element|
       seances = element.search('.txt').text
       if seances.include? '('
         seances_num = seances.scan(/\(([^\)]+)\)/).last.first.to_i
           if  seances_num > 200
-            # and Event.where(name: element.search('.meta-title-link').text).blank?
-          event = {
+
+          # and Event.where(name: element.search('.meta-title-link').text).blank?
+          event = Event.new(
             headline: element.search('.meta-title-link').text,
-            occurs_at: element.search('.date').text,
+            category: categorie,
             sub_category_id: '22',
-            description: seances_num
-            }
-          puts event
+            description: seances_num,
+            occurs_at: '22 avril 2017'
+            )
+          p element.search('.date').text
+          p event.headline
+          p event.valid?
+          if event.headline == "Les Schtroumpfs et le village perdu"
+            binding.pry
+          end
+          p event.errors.full_messages
+          p event.save
           end
       end
     end
