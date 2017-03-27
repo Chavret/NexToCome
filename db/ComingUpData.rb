dca18cfc23a3d45686a213249e4ae0f84fae3400
Simon Baldeyrou <simonbaldeyrou@gmail.com>
2017-03-27 14:17:19 +0200

    meteojob et new database

--- db/ComingUpData.rb
+++
@@ -1,8 +0,0 @@
-require 'csv'
-
-csv_options = { col_sep: ';', quote_char: '"', headers: :first_row }
-filepath    = File.join(__dir__, 'ComingUpDataCSV2.csv')
-
-CSV.foreach(filepath, csv_options) do |row|
-  puts "#{row[0]}: #{row[1]}, Sous-catégorie #{row[8]} (n°: #{row[3]}). Rating: #{row[4]}. Source:#{row[5]}"
-end
