ee44c358a8ad195a9fd781ccdccdb75b9be0e888
Axelle Renie <axelle.renie@gmail.com>
2017-03-29 21:21:36 +0200

    colors

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -103,16 +103,17 @@ class EventsController < ApplicationController
     end
 
     # Meteo Symbols
-    meteo_eventsall = Event.joins(:sub_category).where(sub_categories: { name: 'Météo'})
+    meteo_events = Event.joins(:sub_category).where(sub_categories: { name: 'Météo'})
     @meteo_hash = {}
     date = Time.now.to_date - 1.days
     30.times do
       @meteo_hash[date += 1.day] = []
     end
     @meteo_hash.each do |date, _date_events|
-      @meteo_hash[date] << meteo_eventsall.where(occurs_at: date)
+      @meteo_hash[date] << meteo_events.where(occurs_at: date)
       @meteo_hash[date].flatten!
     end
+
 
   end
 
